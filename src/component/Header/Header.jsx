816d5df987e7596abc98bbccdafe75309d754463
HoseinGholami <139072268+hoseingh7@users.noreply.github.com>
2024-08-01 07:47:01 -0700

    complete header

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,10 +1,14 @@
 import { SiJordan } from "react-icons/si";
+import { SiNike } from "react-icons/si";
+import { CiSearch } from "react-icons/ci";
+import { CiHeart } from "react-icons/ci";
+import { BsBag } from "react-icons/bs";
 
 export const Header = () => {
   return (
     <div className="w-full  flex flex-col ">
       {/* top header */}
-      <div className="bg-[#f5f5f5] flex justify-between px-3 h-10 items-center">
+      <div className="bg-[#f5f5f5] flex justify-between px-10 h-10 items-center">
         <div className="flex gap-2 pl-2 items-center ">
           <SiJordan />
           <a
@@ -31,9 +35,34 @@ export const Header = () => {
             </svg>
           </a>
         </div>
-        <div></div>
+        <div className="flex items-center gap-1 py-3">
+          <div className="border-r border-gray-400  px-2">Find a Store</div>
+          <div className="border-r border-gray-400  px-2">Help</div>
+          <div className="border-r border-gray-400  px-2">Join Us</div>
+          <div className="">Sign In</div>
+        </div>
       </div>
+
       {/* bottom header */}
+
+      <div className="flex justify-between items-center px-10">
+        <div>
+          <SiNike className="w-[80px] h-[80px]" />
+        </div>
+        <div className="flex items-center gap-3 font-bold text-lg">
+          <p>New & Featured</p>
+          <p>Men</p>
+          <p>Women</p>
+          <p>Kide</p>
+          <p>Jordan</p>
+          <p>Sale</p>
+        </div>
+        <div className="flex items-center gap-3">
+          <CiSearch className="w-9 h-9" />
+          <CiHeart className="w-9 h-9" />
+          <BsBag className="w-6 h-6" />
+        </div>
+      </div>
     </div>
   );
 };
