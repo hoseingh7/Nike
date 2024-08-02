66579502eeb84b53fff9237f32699339b41bc7f8
HoseinGholami <139072268+hoseingh7@users.noreply.github.com>
2024-08-02 08:08:32 -0700

    edit in router and add the footer the layout

--- src/layout/Routers.js
+++ src/layout/Routers.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "../page/home/Home";
 import { Header } from "../component/Header/Header";
+import { Footer } from "../component/Footer/Footer";
 
 export const Routers = () => {
   return (
@@ -9,6 +10,7 @@ export const Routers = () => {
       <Routes>
         <Route element={<Home />} path="/" />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
