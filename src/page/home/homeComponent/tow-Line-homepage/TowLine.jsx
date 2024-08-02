d43274697eb4bc6cb2a0d207e154d8771e0a579c
HoseinGholami <139072268+hoseingh7@users.noreply.github.com>
2024-08-02 07:58:26 -0700

    add and editing in home page

---
+++ src/page/home/homeComponent/tow-Line-homepage/TowLine.jsx
@@ -0,0 +1,17 @@
+export const TowLine = () => {
+  return (
+    <div className="flex flex-col justify-center items-center gap-3 pb-10">
+      <h3 className="text-[60px] font-bold">Glow All Out</h3>
+      <p>
+        Jordan Chiles' powerful moves and confidence comes from
+        <br />
+        knowing that the only score that matters is the one you
+        <br />
+        give yourself. See what pushes her to be her best.
+      </p>
+      <button className="w-[90px] h-[40px] bg-black rounded-3xl text-white">
+        Explore
+      </button>
+    </div>
+  );
+};
