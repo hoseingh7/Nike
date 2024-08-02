116279c202d3ad90a27291b4599b6b38ad58cb74
HoseinGholami <139072268+hoseingh7@users.noreply.github.com>
2024-08-02 08:01:32 -0700

    add the classic shop in home page

---
+++ src/page/home/homeComponent/Shop-Classics/ShopClassics.jsx
@@ -0,0 +1,12 @@
+import { Slider } from "./Slider/Slider";
+
+export const ShopClassics = () => {
+  return (
+    <div className="pb-14">
+      <h1 className="text-[40px] font-black text-left pl-8 pb-8 fw">
+        SHOP BY CLASSICS
+      </h1>
+      <Slider />
+    </div>
+  );
+};
---
+++ src/page/home/homeComponent/Shop-Classics/Slider/Slider.jsx
@@ -0,0 +1,72 @@
+import React, { useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Pagination, Navigation } from "swiper/modules";
+import one from "../../../../../images/nike-just-do-it (7).jpg";
+import two from "../../../../../images/nike-just-do-it (8).jpg";
+import three from "../../../../../images/nike-just-do-it (9).jpg";
+import four from "../../../../../images/nike-just-do-it (10).jpg";
+import five from "../../../../../images/nike-just-do-it (11).jpg";
+import six from "../../../../../images/nike-just-do-it (12).jpg";
+
+import "./slider.css";
+
+export const Slider = () => {
+  const [swiperRef, setSwiperRef] = useState(null);
+  const dataSlider = [
+    {
+      image: one,
+      title: "Air Jordan 1",
+    },
+    {
+      image: two,
+      title: "Dunk",
+    },
+    {
+      image: three,
+      title: "Air Force 1",
+    },
+    {
+      image: four,
+      title: "Blazer",
+    },
+    {
+      image: five,
+      title: "Vomero",
+    },
+    {
+      image: six,
+      title: "Air Max",
+    },
+  ];
+
+  return (
+    <>
+      <Swiper
+        onSwiper={setSwiperRef}
+        slidesPerView={4}
+        centeredSlides={true}
+        spaceBetween={10}
+        loop={true}
+        pagination={{
+          type: "fraction",
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        {dataSlider.map((item, index) => (
+          <SwiperSlide key={index}>
+            <div className="slider-box">
+              <img src={item.image} alt={item.title} className="slider-image" />
+              {/* <h3 className="slider-title">{item.title}</h3> */}
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
+  );
+};
