6f132c9a9e48672ac85cd0cc9f56e69fc161d47b
HoseinGholami <139072268+hoseingh7@users.noreply.github.com>
2024-08-02 08:07:16 -0700

    editing the banner

--- src/page/home/homeComponent/banner-in-header/Banner.jsx
+++ src/page/home/homeComponent/banner-in-header/Banner.jsx
@@ -1,9 +1,11 @@
 import karim from "../../../../images/nike-just-do-it.jpg";
+// import Karim from "./nike-just-do-it.jpg";
 
 export const Banner = () => {
   return (
     <div>
-      <img src={karim} alt="banner" />
+      {/* <img src={karim} alt="banner" /> */}
+      <img src={karim} alt="" />
     </div>
   );
 };
