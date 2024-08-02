b88e921f6185bb1ab52da9905354516d6f8b60cc
HoseinGholami <139072268+hoseingh7@users.noreply.github.com>
2024-08-02 08:07:42 -0700

    edit home page

--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -1,9 +1,21 @@
+import { FooterHome } from "./homeComponent/Footer-in-HomePage/FooterHome";
+import { NewsWeek } from "./homeComponent/New-this-week/NewWeek";
+import { ShopClassics } from "./homeComponent/Shop-Classics/ShopClassics";
 import { Banner } from "./homeComponent/banner-in-header/Banner";
+import { ShoeType } from "./homeComponent/shoe-type/ShoeType";
+import { TowLine } from "./homeComponent/tow-Line-homepage/TowLine";
+import { VideoLine } from "./homeComponent/video-Line/VideoLine";
 
 export const Home = () => {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col pb-36">
       <Banner />
+      <TowLine />
+      <ShoeType />
+      <VideoLine />
+      <NewsWeek />
+      <ShopClassics />
+      <FooterHome />
     </div>
   );
 };
