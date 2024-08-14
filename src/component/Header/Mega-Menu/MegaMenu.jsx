3eefbf58370ae06948f817509fb6e36ded6114f7
HoseinGholami <139072268+hoseingh7@users.noreply.github.com>
2024-08-14 03:41:40 -0700

    Mega Menu creation and still on completion

---
+++ src/component/Header/Mega-Menu/MegaMenu.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { menus } from "./data";
+const MegaMenu = () => {
+  const [activeMenu, setActiveMenu] = useState(null);
+  console.log("menu", menus);
+  return (
+    <div className="flex">
+      {menus.map((menu, index) => (
+        <div
+          key={index}
+          className="inline-block "
+          onMouseEnter={() => setActiveMenu(index)}
+          onMouseLeave={() => setActiveMenu(null)}
+        >
+          <button className="px-4 py-2">{menu.title}</button>
+          {activeMenu === index && (
+            <div className="absolute left-1/2 transform -translate-x-1/2  w-screen bg-red-300 border flex flex-wrap justify-start gap-6 items-centen text-start">
+              <div className="p-4 flex mx-auto flex-wrap">
+                {menu.subMenus.map((subMenu, subIndex) => (
+                  <div key={subIndex} className="mb-2">
+                    <h4 className="font-bold">{subMenu.title}</h4>
+                    <ul>
+                      {subMenu.items.map((item, itemIndex) => (
+                        <li key={itemIndex}>{item}</li>
+                      ))}
+                    </ul>
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default MegaMenu;
---
+++ src/component/Header/Mega-Menu/data.js
@@ -0,0 +1,312 @@
+export const menus = [
+  {
+    title: "New & Featured",
+    subMenus: [
+      {
+        title: " Get Set for School",
+        items: [
+          "Extra 25% Off: New Styles Added",
+          "Shop the Collection",
+          "Shopping Guide",
+          "Bags & Backpacks",
+        ],
+      },
+      {
+        title: "New & Featured",
+        items: ["New Arrivals,", "Best Sellers", "New & Upcoming Drops"],
+      },
+      {
+        title: "Trending",
+        items: ["ACG", "Retro Running", "Aster Pink Shop", "Shop Sport"],
+      },
+      {
+        title: "Shop Classics",
+        items: [
+          "Dunk",
+          "Air Jordan 1",
+          "Air Force",
+          "Air Max",
+          "Blazer",
+          "Vomero",
+        ],
+      },
+      {
+        title: "Explore",
+        items: [
+          "SNKRS Launch Calendar",
+          "Running Shoe Finder",
+          "Bra Finder",
+          "Product Care",
+          "Member Rewards",
+          "Buying Guides",
+        ],
+      },
+    ],
+  },
+  {
+    title: "Men",
+    subMenus: [
+      {
+        title: "Get Set for School",
+        items: [
+          "Extra 25% Off: New Styles Added",
+          "Shop the Collection",
+          "Shopping Guide",
+          "Bags & Backpacks",
+        ],
+      },
+
+      {
+        title: "New & Featured",
+        items: [
+          "New Arrivals",
+          "Best Sellers",
+          "New & Upcoming Drops",
+          "All Sale",
+        ],
+      },
+      {
+        title: "Collections",
+        items: ["ACG Essentials", "Soccer Cleats", "Nike Life"],
+      },
+
+      {
+        title: "Shoes",
+        items: [
+          "All Shoes",
+          "Lifestyle",
+          "Jordan",
+          "Dunk",
+          "Retro Running",
+          "Air Max",
+          "Air Force 1",
+          "Training & Gym",
+          "Basketball",
+          "Running",
+          "Nike SB",
+          "Sandals & Slides",
+          "Nike by You",
+          "Shoes $100 & Under",
+        ],
+      },
+      {
+        title: "Clothing",
+        items: [
+          "All Clothing",
+          "Hoodies & Sweatshirts",
+          "Shorts",
+          "Pants & Tights",
+          "Jackets & Vests",
+          "Tops & T-Shirts",
+          "Matching Sets",
+          "Big & Tall",
+          "Underwear & More",
+          "Jordan",
+        ],
+      },
+      {
+        title: "Accessories",
+        items: [
+          "All Accessories",
+          "Bags & Backpacks",
+          "Hats & Headwear",
+          "Sunglasses",
+          "Socks",
+        ],
+      },
+      {
+        title: "Shop by Sport",
+        items: [
+          "Basketball",
+          "Running",
+          "Golf",
+          "Soccer",
+          "Training & Gym",
+          "Tennis",
+          "Baseball",
+          "Football",
+          "Trail Running",
+          "Swimming",
+          "Pickleball",
+          "Fan Gear",
+        ],
+      },
+    ],
+  },
+  {
+    title: "Women",
+    subMenus: [
+      {
+        title: " Get Set for School",
+        items: [
+          "Extra 25% Off: New Styles Added",
+          "Shop the Collection",
+          "Shopping Guide",
+          "Bags & Backpacks",
+        ],
+      },
+      {
+        title: "New & Featured",
+        items: ["New Arrivals,", "Best Sellers", "New & Upcoming Drops"],
+      },
+      {
+        title: "Trending",
+        items: ["ACG", "Retro Running", "Aster Pink Shop", "Shop Sport"],
+      },
+      {
+        title: "Shop Classics",
+        items: [
+          "Dunk",
+          "Air Jordan 1",
+          "Air Force",
+          "Air Max",
+          "Blazer",
+          "Vomero",
+        ],
+      },
+      {
+        title: "Explore",
+        items: [
+          "SNKRS Launch Calendar",
+          "Running Shoe Finder",
+          "Bra Finder",
+          "Product Care",
+          "Member Rewards",
+          "Buying Guides",
+        ],
+      },
+    ],
+  },
+  {
+    title: "Kids",
+    subMenus: [
+      {
+        title: " Get Set for School",
+        items: [
+          "Extra 25% Off: New Styles Added",
+          "Shop the Collection",
+          "Shopping Guide",
+          "Bags & Backpacks",
+        ],
+      },
+      {
+        title: "New & Featured",
+        items: ["New Arrivals,", "Best Sellers", "New & Upcoming Drops"],
+      },
+      {
+        title: "Trending",
+        items: ["ACG", "Retro Running", "Aster Pink Shop", "Shop Sport"],
+      },
+      {
+        title: "Shop Classics",
+        items: [
+          "Dunk",
+          "Air Jordan 1",
+          "Air Force",
+          "Air Max",
+          "Blazer",
+          "Vomero",
+        ],
+      },
+      {
+        title: "Explore",
+        items: [
+          "SNKRS Launch Calendar",
+          "Running Shoe Finder",
+          "Bra Finder",
+          "Product Care",
+          "Member Rewards",
+          "Buying Guides",
+        ],
+      },
+    ],
+  },
+  {
+    title: "Jordan",
+    subMenus: [
+      {
+        title: " Get Set for School",
+        items: [
+          "Extra 25% Off: New Styles Added",
+          "Shop the Collection",
+          "Shopping Guide",
+          "Bags & Backpacks",
+        ],
+      },
+      {
+        title: "New & Featured",
+        items: ["New Arrivals,", "Best Sellers", "New & Upcoming Drops"],
+      },
+      {
+        title: "Trending",
+        items: ["ACG", "Retro Running", "Aster Pink Shop", "Shop Sport"],
+      },
+      {
+        title: "Shop Classics",
+        items: [
+          "Dunk",
+          "Air Jordan 1",
+          "Air Force",
+          "Air Max",
+          "Blazer",
+          "Vomero",
+        ],
+      },
+      {
+        title: "Explore",
+        items: [
+          "SNKRS Launch Calendar",
+          "Running Shoe Finder",
+          "Bra Finder",
+          "Product Care",
+          "Member Rewards",
+          "Buying Guides",
+        ],
+      },
+    ],
+  },
+  {
+    title: "Sale",
+    subMenus: [
+      {
+        title: " Get Set for School",
+        items: [
+          "Extra 25% Off: New Styles Added",
+          "Shop the Collection",
+          "Shopping Guide",
+          "Bags & Backpacks",
+        ],
+      },
+      {
+        title: "New & Featured",
+        items: ["New Arrivals,", "Best Sellers", "New & Upcoming Drops"],
+      },
+      {
+        title: "Trending",
+        items: ["ACG", "Retro Running", "Aster Pink Shop", "Shop Sport"],
+      },
+      {
+        title: "Shop Classics",
+        items: [
+          "Dunk",
+          "Air Jordan 1",
+          "Air Force",
+          "Air Max",
+          "Blazer",
+          "Vomero",
+        ],
+      },
+      {
+        title: "Explore",
+        items: [
+          "SNKRS Launch Calendar",
+          "Running Shoe Finder",
+          "Bra Finder",
+          "Product Care",
+          "Member Rewards",
+          "Buying Guides",
+        ],
+      },
+    ],
+  },
+];
