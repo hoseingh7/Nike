f57048550da4d2aa6f5bc9256dfd373f027ed5eb
HoseinGholami <139072268+hoseingh7@users.noreply.github.com>
2024-08-14 03:45:38 -0700

    edditing the router componnent and add the products page

--- src/layout/Routers.js
+++ src/layout/Routers.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "../page/home/Home";
 import { Header } from "../component/Header/Header";
 import { Footer } from "../component/Footer/Footer";
+import { Products } from "../page/Products/Products";
 
 export const Routers = () => {
   return (
@@ -9,6 +10,7 @@ export const Routers = () => {
       <Header />
       <Routes>
         <Route element={<Home />} path="/" />
+        <Route element={<Products />} path="/products" />
       </Routes>
       <Footer />
     </BrowserRouter>
