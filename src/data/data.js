bc97a78a89f5bc57b17cae07fb451dac0bb39afd
HoseinGholami <139072268+hoseingh7@users.noreply.github.com>
2024-08-16 22:35:23 -0700

    in progress

---
+++ src/data/data.js
@@ -0,0 +1,255 @@
+import one from "../images/show-shop/air-vapormax-2023-flyknit-electric-mens-shoes-vSm5p2.jpeg";
+import two from "../images/show-shop/air-vapormax-moc-roam-electric-mens-shoes-0djDxd.jpeg";
+import three from "../images/show-shop/alphafly-3-electric-mens-road-racing-shoes-5gZBgL.jpeg";
+import four from "../images/show-shop/alphafly-3-electric-womens-road-racing-shoes-LQX5b4.jpeg";
+import five from "../images/show-shop/dragoy-2-electric-track-field-distance-spikes-03rvtN.jpeg";
+import six from "../images/show-shop/dragoy-2-elite-electric-track-field-distance-spikes-4C2Kbs.png";
+import seven from "../images/show-shop/gp-challenge-1-electric-mens-hard-court-tennis-shoes-cFdcc5.jpeg";
+import eight from "../images/show-shop/gt-cut-3-electric-basketball-shoes-GLHCGc.jpeg";
+import nine from "../images/show-shop/gt-jump-2-electric-mens-basketball-shoes-CZpmDQ.jpeg";
+import ten from "../images/show-shop/infinity-tour-2-electric-golf-shoes-HH7SLD.jpeg";
+import ele from "../images/show-shop/infinityrn-4-electric-mens-road-running-shoes-hlqHFQ.jpeg";
+import twoele from "../images/show-shop/infinityrn-4-electric-womens-road-running-shoes-k1GRZP.jpeg";
+import threle from "../images/show-shop/invincible-3-electric-mens-road-running-shoes-6MqQ72.jpeg";
+import fourele from "../images/show-shop/invincible-3-electric-womens-road-running-shoes-tfL5Tr.jpeg";
+import fivele from "../images/show-shop/maxfly-2-electric-track-field-sprinting-spikes-fBfStC.jpeg";
+import sevele from "../images/show-shop/mercurial-superfly-10-elite-blueprint-fg-high-top-soccer-cleats-4TH922.jpeg";
+import tt from "../images/show-shop/nike-just-do-it (7).jpg";
+import aa from "../images/show-shop/nikecourt-air-zoom-vapor-11-electric-mens-hard-court-tennis-shoes-pG5rtb.jpeg";
+import bb from "../images/show-shop/pegasus-41-electric-mens-road-running-shoes-7S90QB.jpeg";
+import cc from "../images/show-shop/pegasus-41-electric-womens-road-running-shoes-tSbZGh.jpeg";
+import dd from "../images/show-shop/pegasus-easyon-electric-womens-road-running-shoes-J8Rvp9.jpeg";
+import ff from "../images/show-shop/vaporfly-3-electric-road-racing-shoes-8DD182.jpeg";
+import gg from "../images/show-shop/vaporfly-3-electric-womens-road-racing-shoes-4jJ4vQ.jpeg";
+import hh from "../images/show-shop/victory-2-electric-track-field-distance-spikes-Xb1b5L.jpeg";
+import ii from "../images/show-shop/victory-tour-3-electric-golf-shoes-XNbrg3.jpeg";
+export const items = [
+  {
+    image: one,
+    category: "airjordan",
+    count: 10,
+    name: "Nike Air VaporMax 2023 Flyknit Electric",
+    feture: "Men's Shoes",
+    color: ["red", "blue", "green"],
+    price: "$220",
+  },
+  {
+    image: two,
+    category: "airjordan",
+    count: 10,
+    name: "Nike Alphafly 3 Electric",
+    feture: "Men's Road Racing Shoes",
+    color: ["red", "blue", "green"],
+    price: "$330",
+  },
+  {
+    image: three,
+    category: "airjordan",
+    count: 10,
+    name: "Nike Alphafly 3 Electric",
+    feture: "Men's Road Racing Shoes",
+    color: ["red", "blue", "green"],
+    price: "$300",
+  },
+  {
+    image: four,
+    category: "airjordan",
+    count: 10,
+    name: "Nike Alphafly 3 Electric",
+    feture: "Men's Road Racing Shoes",
+    color: ["red", "blue", "green"],
+    price: "$330",
+  },
+  {
+    image: five,
+    category: "airjordan",
+    count: 10,
+    name: "Nike Dragonfly 2 Elite Electric",
+    feture: "Track & Field Distance Spikes",
+    color: ["red", "blue", "green"],
+    price: "$240",
+  },
+  {
+    image: six,
+    category: "airjordan",
+    count: 10,
+    name: "Nike Dragonfly 2 Elite Electric",
+    feture: "Track & Field Distance Spikes",
+    color: ["red", "blue", "green"],
+    price: "$240",
+  },
+  {
+    image: seven,
+    category: "airjordan",
+    count: 10,
+    name: "Nike Invincible 3 Electric",
+    feture: "Men's Road Running Shoes",
+    color: ["red", "blue", "green"],
+    price: "$195",
+  },
+  {
+    image: eight,
+    category: "airjordan",
+    count: 10,
+    name: "Nike G.T. Cut 3 Electric",
+    feture: "Basketball Shoes",
+    color: ["red", "blue", "green"],
+    price: "$205",
+  },
+  {
+    image: nine,
+    category: "airjordan",
+    count: 10,
+    name: "Nike G.T. Jump 2 Electric",
+    feture: "Men's Basketball Shoes",
+    color: ["red", "blue", "green"],
+    price: "$195",
+  },
+  {
+    image: ten,
+    category: "airjordan",
+    count: 10,
+    name: "Nike Alphafly 3 Electric",
+    feture: "Men's Road Racing Shoes",
+    color: ["red", "blue", "green"],
+    price: "$330",
+  },
+  {
+    image: ele,
+    category: "airjordan",
+    count: 10,
+    name: "Nike InfinityRN 4 Electric",
+    feture: "Women's Road Running Shoes",
+    color: ["red", "blue", "green"],
+    price: "$190",
+  },
+  {
+    image: twoele,
+    category: "airjordan",
+    count: 10,
+    name: "Nike InfinityRN 4 Electric",
+    feture: "Men's Road Running Shoes",
+    color: ["red", "blue", "green"],
+    price: "$175",
+  },
+  {
+    image: threle,
+    category: "airjordan",
+    count: 10,
+    name: "Nike Alphafly 3 Electric",
+    feture: "Men's Road Racing Shoes",
+    color: ["red", "blue", "green"],
+    price: "$330",
+  },
+  {
+    image: fourele,
+    category: "airjordan",
+    count: 10,
+    name: "Nike Alphafly 3 Electric",
+    feture: "Men's Road Racing Shoes",
+    color: ["red", "blue", "green"],
+    price: "$330",
+  },
+  {
+    image: fivele,
+    category: "airjordan",
+    count: 10,
+    name: "Nike Alphafly 3 Electric",
+    feture: "Men's Road Racing Shoes",
+    color: ["red", "blue", "green"],
+    price: "$330",
+  },
+
+  {
+    image: sevele,
+    category: "airjordan",
+    count: 10,
+    name: "Nike Alphafly 3 Electric",
+    feture: "Men's Road Racing Shoes",
+    color: ["red", "blue", "green"],
+    price: "$330",
+  },
+
+  {
+    image: tt,
+    category: "airjordan",
+    count: 10,
+    name: "Nike Alphafly 3 Electric",
+    feture: "Men's Road Racing Shoes",
+    color: ["red", "blue", "green"],
+    price: "$330",
+  },
+  {
+    image: aa,
+    category: "airjordan",
+    count: 10,
+    name: "Nike Alphafly 3 Electric",
+    feture: "Men's Road Racing Shoes",
+    color: ["red", "blue", "green"],
+    price: "$330",
+  },
+  {
+    image: bb,
+    category: "airjordan",
+    count: 10,
+    name: "Nike Alphafly 3 Electric",
+    feture: "Men's Road Racing Shoes",
+    color: ["red", "blue", "green"],
+    price: "$330",
+  },
+  {
+    image: cc,
+    category: "airjordan",
+    count: 10,
+    name: "Nike Alphafly 3 Electric",
+    feture: "Men's Road Racing Shoes",
+    color: ["red", "blue", "green"],
+    price: "$330",
+  },
+  {
+    image: dd,
+    category: "airjordan",
+    count: 10,
+    name: "Nike Alphafly 3 Electric",
+    feture: "Men's Road Racing Shoes",
+    color: ["red", "blue", "green"],
+    price: "$330",
+  },
+
+  {
+    image: ff,
+    category: "airjordan",
+    count: 10,
+    name: "Nike Alphafly 3 Electric",
+    feture: "Men's Road Racing Shoes",
+    color: ["red", "blue", "green"],
+    price: "$330",
+  },
+  {
+    image: gg,
+    category: "airjordan",
+    count: 10,
+    name: "Nike Alphafly 3 Electric",
+    feture: "Men's Road Racing Shoes",
+    color: ["red", "blue", "green"],
+    price: "$330",
+  },
+  {
+    image: hh,
+    category: "airjordan",
+    count: 10,
+    name: "Nike Alphafly 3 Electric",
+    feture: "Men's Road Racing Shoes",
+    color: ["red", "blue", "green"],
+    price: "$330",
+  },
+  {
+    image: ii,
+    category: "airjordan",
+    count: 10,
+    name: "Nike Alphafly 3 Electric",
+    feture: "Men's Road Racing Shoes",
+    color: ["red", "blue", "green"],
+    price: "$330",
+  },
+];
---
+++ src/data/func.js
@@ -0,0 +1,13 @@
+import asqar from "../images/show-shop/from-the-archives-the-evolution-of-nike-air-mag.jpg";
+
+export const items = [
+  {
+    image: asqar,
+    category: "airjordan",
+    count: 10,
+    name: "Nike Alphafly 3 Electric",
+    feture: "Men's Road Racing Shoes",
+    color: ["red", "blue", "green"],
+    price: "$330",
+  },
+];
