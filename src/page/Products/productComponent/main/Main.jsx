45ea0efef2a1d7c6e2b629d74b8ac236407d7adf
HoseinGholami <139072268+hoseingh7@users.noreply.github.com>
2024-08-16 22:33:19 -0700

    in progress

--- src/page/Products/productComponent/main/Main.jsx
+++ src/page/Products/productComponent/main/Main.jsx
@@ -6,8 +6,17 @@ export const Main = ({ showFilters }) => {
     <div className="w-full flex">
       <div className="w-full flex">
         {showFilters && <Left showFilters={showFilters} />}
-        <Right className={showFilters ? "w-4/5" : "w-full"} />
+        <Right
+          className={
+            showFilters ? "w-4/5 duration-700" : "w-full  duration-700"
+          }
+        />
       </div>
     </div>
   );
 };
+// className={
+//   showFilters
+//     ? "w-4/5 transition-all duration-500"
+//     : "w-full transition-all duration-500"
+// }
