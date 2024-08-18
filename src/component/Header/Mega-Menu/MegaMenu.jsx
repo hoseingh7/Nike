92deff1919a1f2fa8ffce49b38931009bc3b3a7e
HoseinGholami <139072268+hoseingh7@users.noreply.github.com>
2024-08-17 22:21:55 -0700

    editting in mega menu

--- src/component/Header/Mega-Menu/MegaMenu.jsx
+++ src/component/Header/Mega-Menu/MegaMenu.jsx
@@ -14,14 +14,19 @@ const MegaMenu = () => {
         >
           <button className="px-4 py-2">{menu.title}</button>
           {activeMenu === index && (
-            <div className="absolute left-1/2 transform -translate-x-1/2  w-screen bg-red-300 border flex flex-wrap justify-start gap-6 items-centen text-start">
-              <div className="p-4 flex mx-auto flex-wrap">
+            <div className="absolute left-1/2 transform -translate-x-1/2  w-screen bg-white border flex flex-wrap justify-between gap-20 items-center text-start ">
+              <div className="p-4 flex mx-auto flex-wrap gap-10">
                 {menu.subMenus.map((subMenu, subIndex) => (
                   <div key={subIndex} className="mb-2">
                     <h4 className="font-bold">{subMenu.title}</h4>
                     <ul>
                       {subMenu.items.map((item, itemIndex) => (
-                        <li key={itemIndex}>{item}</li>
+                        <li
+                          key={itemIndex}
+                          className="text-gray-400 text-[15px]"
+                        >
+                          {item}
+                        </li>
                       ))}
                     </ul>
                   </div>
