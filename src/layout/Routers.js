fc3650f801099be5a68a18dd89aea50b742498db
HoseinGholami <139072268+hoseingh7@users.noreply.github.com>
2024-08-18 01:29:30 -0700

    edie the route component and adding route for product info page

--- src/layout/Routers.js
+++ src/layout/Routers.js
@@ -3,6 +3,7 @@ import { Home } from "../page/home/Home";
 import { Header } from "../component/Header/Header";
 import { Footer } from "../component/Footer/Footer";
 import { Products } from "../page/Products/Products";
+import { ProductInfo } from "../page/Product-info/ProductInfo";
 
 export const Routers = () => {
   return (
@@ -11,6 +12,7 @@ export const Routers = () => {
       <Routes>
         <Route element={<Home />} path="/" />
         <Route element={<Products />} path="/products" />
+        <Route element={<ProductInfo />} path="/pro-info" />
       </Routes>
       <Footer />
     </BrowserRouter>
