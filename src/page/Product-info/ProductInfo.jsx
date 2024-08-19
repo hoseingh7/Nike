8efcd86f26198b24e4750b48a83f5417be7e0de7
HoseinGholami <139072268+hoseingh7@users.noreply.github.com>
2024-08-18 21:22:17 -0700

    in progress

--- src/page/Product-info/ProductInfo.jsx
+++ src/page/Product-info/ProductInfo.jsx
@@ -1,9 +1,10 @@
 import { MainTop } from "./component/mainTop/MainTop";
+import { Slider } from "./component/slider/Slider";
 
 export const ProductInfo = () => {
   return (
-    <div className="productInfo">
-      <MainTop />{" "}
+    <div className="flex flex-col">
+      <MainTop /> <Slider />
     </div>
   );
 };
