9f3c498440ee243175649c11e83c0cc37d42fea0
HoseinGholami <139072268+hoseingh7@users.noreply.github.com>
2024-08-20 00:53:07 -0700

    edit the router component and adding the dashboard page

--- src/layout/Routers.js
+++ src/layout/Routers.js
@@ -1,16 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "../page/home/Home";
-import { Header } from "../component/Header/Header";
-import { Footer } from "../component/Footer/Footer";
 import { Products } from "../page/Products/Products";
 import { ProductInfo } from "../page/Product-info/ProductInfo";
 import { Landing } from "../page/landing/Landing";
 import { Layout } from "./Layout";
+import { Dashboard } from "../Dashboard/Dashboard";
 
 export const Routers = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route element={<Dashboard />} path="/dash" />
         <Route path="/landing" element={<Landing />} />
         <Route element={<Layout />}>
           <Route element={<Home />} path="/" />
